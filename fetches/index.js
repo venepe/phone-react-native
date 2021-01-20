15797c94145d2acdc968a6f1efafc0fff27e6feb
venepe <venepe@users.noreply.github.com>
2021-01-19 22:59:48 -0600

    Add fetches

--- fetches/index.js
+++ fetches/index.js
@@ -9,3 +9,96 @@ export const getAvailableNumbers = ({ latitude, longitude }) => {
     },
   })
 };
+
+export const getAccounts = ({ token }) => {
+  return fetch(`${API_URL}/accounts`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  })
+};
+
+export const postAccounts = ({ token, phoneNumber }) => {
+  return fetch(`${API_URL}/accounts`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      account: {
+        phoneNumber,
+      },
+    }),
+  })
+};
+
+export const postInvitation = ({ token, phoneNumber }) => {
+  return fetch(`${API_URL}/accounts/${phoneNumber}/invitations`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  })
+};
+
+export const postOwn = ({ token, phoneNumber, code }) => {
+  return fetch(`${API_URL}/accounts/${phoneNumber}/owns`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      own: {
+        code,
+      },
+    }),
+  })
+};
+
+export const getMessages = ({ token, phoneNumber }) => {
+  return fetch(`${API_URL}/accounts/${phoneNumber}/messages`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  })
+};
+
+export const postMessage = ({ token, phoneNumber, to, body }) => {
+  return fetch(`${API_URL}/accounts/${phoneNumber}/messages`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      message: {
+        to,
+        body,
+      },
+    }),
+  })
+};
+
+export const getCalls = ({ token, phoneNumber }) => {
+  return fetch(`${API_URL}/accounts/${phoneNumber}/calls`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  })
+};

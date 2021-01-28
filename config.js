11aeead0de5f0b4dc3a6c81898e83ad572c44ebc
venepe <venepe@users.noreply.github.com>
2021-01-27 23:22:20 -0600

    Add apple sign on

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
 
-export const API_URL = 'http://192.168.0.24:8002';
-export const GRAPHQL_URL = `${API_URL}/graphql`;
+export const API_URL = 'https://api.anumberforus.com';
 export const AUTH0_DOMAIN = 'bubblepop.us.auth0.com';
 export const AUTH0_CLIENT_ID = 'LCGTl08HZL5Se6baHXZHSeGild39j4HL';
 export const AUTH0_AUDIENCE = 'https://bubblepop.io/api/v1/';
 export const MIXPANEL_TOKEN = '';
 
-// https://api.getbubblepop.com
+// https://api.anumberforus.com
 // http://localhost:8002
 // http://192.168.0.24:8002

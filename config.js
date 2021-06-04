d1ffafa978521adeb65858f81d9ac34d9a71ee2f
venepe <venepe@users.noreply.github.com>
2021-06-03 19:16:06 -0500

    Release. Change target

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 
-export const API_URL = 'https://c932f04c618c.ngrok.io';
+export const API_URL = 'https://api.pairmeum.com';
 export const SOCKET_URL = API_URL;
 export const AUTH0_DOMAIN = 'bubblepop.us.auth0.com';
 export const AUTH0_CLIENT_ID = 'LCGTl08HZL5Se6baHXZHSeGild39j4HL';
